0cb7029f2f25894d15c411ad2c0e386af0c855cc
Diegodenac <diegocanedo917@gmail.com>
2024-02-15 08:51:51 -0500

    Primeras dos funcionalidades

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,14 @@
-import sumar from "./sumador";
+import saludar from "./saludador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const name = document.querySelector("#user-name");
+const form = document.querySelector("#askName-form");
+const div = document.querySelector("#grettingsName-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const userName = name.value;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p> Hola " + saludar(userName) + "</p>";
 });
+
