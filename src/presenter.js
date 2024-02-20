23f22b8a6d6be91de133ce0adb72b42a3067fef7
Diegodenac <diegocanedo917@gmail.com>
2024-02-19 21:53:40 -0500

    Saludador en ingles y espaniol implementado

--- src/presenter.js
+++ src/presenter.js
@@ -5,6 +5,7 @@ const form = document.querySelector("#askName-form");
 const div = document.querySelector("#grettingsName-div");
 const age = document.querySelector("#user-age")
 const genero = document.querySelector("#gender")
+const language = document.querySelector("#language")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,6 +13,7 @@ form.addEventListener("submit", (event) => {
   const userName = name.value;
   const userAge = parseInt(age.value);
   const userGenero = genero.value;
-  div.innerHTML = "<p>" + saludar(userName, userAge, userGenero) + "</p>";
+  const userLanguage = language.value;
+  div.innerHTML = "<p>" + saludar(userName, userAge, userGenero, userLanguage) + "</p>";
 });
 
--- src/saludador.js
+++ src/saludador.js
@@ -1,26 +1,43 @@
-function saludar(name, age, genero) {
+function saludar(name, age, genero, language) {
   let fechaActual = new Date();
   let message;
-  if (fechaActual.getHours() >= 1){
-    message = 'Buenos dias, '
-    if (fechaActual.getHours() >= 12){
-      message = 'Buenas tardes, '
-      if (fechaActual.getHours() >= 18){
-        message = 'Buenas noches, '
+  if (language == 'EN') {
+    if (fechaActual.getHours() >= 1) {
+      message = 'Good morning, ';
+      if (fechaActual.getHours() >= 12) {
+        message = 'Good afternoon, ';
+        if (fechaActual.getHours() >= 18){
+          message = 'Good night, '
+        }
+      }
+    } 
+    if (parseInt(age) > 30 && genero != "other") {
+      if (genero == "female") {
+        message = message + 'Mrs. ';
+      } else if (genero == "male") {
+        message = message + 'Mr. ';
       }
     }
   }
-  if (parseInt(age) > 30 && genero != "other") {
-    if (genero == "female") {
-      message = message + `Sra ${name}`;
-    } else if (genero == "male") {
-      message = message + `Sr ${name}`;
+  else{
+    if (fechaActual.getHours() >= 1){
+      message = 'Buenos dias, '
+      if (fechaActual.getHours() >= 12){
+        message = 'Buenas tardes, '
+        if (fechaActual.getHours() >= 18){
+          message = 'Buenas noches, '
+        }
+      }
+    }
+    if (parseInt(age) > 30 && genero != "other") {
+      if (genero == "female") {
+        message = message + 'Sra. ';
+      } else if (genero == "male") {
+        message = message + 'Sr. ';
+      }
     }
   }
-  else{
-    message = message + `${name}`;
-  }
-  return message;
+  return message + `${name}`;
 }
 
 export default saludar;
