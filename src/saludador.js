686cba271af32d5122aa857c7436d94b73dab7ab
Diegodenac <diegocanedo917@gmail.com>
2024-02-19 21:32:59 -0500

    Funcionalidad saludar segun la hora implementada

--- src/saludador.js
+++ src/saludador.js
@@ -1,11 +1,24 @@
 function saludar(name, age, genero) {
-  let message = `Hola ${name}`;
-  if (parseInt(age) > 30) {
-    if (genero === "female") {
-      message = `Hola Sra ${name}`;
-    } else if (genero === "male") {
-      message = `Hola Sr ${name}`;
+  let fechaActual = new Date();
+  let message;
+  if (fechaActual.getHours() >= 1){
+    message = 'Buenos dias, '
+    if (fechaActual.getHours() >= 12){
+      message = 'Buenas tardes, '
+      if (fechaActual.getHours() >= 18){
+        message = 'Buenas noches, '
+      }
     }
+  }
+  if (parseInt(age) > 30 && genero != "other") {
+    if (genero == "female") {
+      message = message + `Sra ${name}`;
+    } else if (genero == "male") {
+      message = message + `Sr ${name}`;
+    }
+  }
+  else{
+    message = message + `${name}`;
   }
   return message;
 }
